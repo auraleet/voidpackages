1ca21a885d3697a8f5f8515661a7178862292bd0
aura <93157696+auraleet@users.noreply.github.com>
2021-11-01 04:43:16 +0000

    Add files via upload

--- void.sh
+++ void.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 sudo xbps-install -S vulkan-loader vulkan-loader-32bit Vulkan-Tools vkd3d vkd3d-32bit MangoHud MangoHud-32bit nvidia nvidia-libs-32bit libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit steam pipewire alsa-pipewire file-roller xfce4-screenshooter flatpak python3 python3-pip python-pip neofetch

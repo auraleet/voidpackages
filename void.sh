ce4abc1ccdfdbcf59f2f5d0fbfff03e632405a4e
aura <93157696+auraleet@users.noreply.github.com>
2021-11-01 04:08:11 -0400

    Add files via upload

--- void.sh
+++ void.sh
@@ -1,3 +1,4 @@
-sudo xbps-install -S vulkan-loader vulkan-loader-32bit Vulkan-Tools vkd3d vkd3d-32bit MangoHud MangoHud-32bit nvidia pipewire alsa-pipewire alsa-utils file-roller xfce4-screenshooter flatpak python3 python3-pip python-pip neofetch neovim kitty Thunar fish-shell firefox 
+sudo xbps-install -S vulkan-loader vulkan-loader-32bit Vulkan-Tools nvidia pipewire alsa-pipewire alsa-utils file-roller xfce4-screenshooter flatpak python3 python3-pip python-pip neofetch neovim kitty Thunar fish-shell firefox 
+h neovim kitty Thunar fish-shell firefox 
 n3 python3-pip python-pip neofetch neovim kitty Thunar fish-shell firefox 
 etch neovim kitty Thunar fish-shell firefox 
